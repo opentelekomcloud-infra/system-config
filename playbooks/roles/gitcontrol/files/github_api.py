c3a0717b0751a688a06b390c67089759ec94f7e3
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2021-05-14 12:13:25 +0300

    members and teams endpoints (#117)
    members and teams endpoints
    
    currently added endpoint for invitation to orgs
    and teams management
    
    Reviewed-by: None <None>
    Reviewed-by: Artem Goncharov <Artem.goncharov@gmail.com>
    Reviewed-by: Anton Sidelnikov <None>

--- playbooks/roles/gitcontrol/files/github_api.py
+++ playbooks/roles/gitcontrol/files/github_api.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 import os
 import sys
 from argparse import ArgumentParser
@@ -8,6 +9,7 @@ import yaml
 
 github_user = os.getenv('GITHUB_USER')
 github_token = os.getenv('GITHUB_TOKEN')
+template_folder = '/home/zuul/src/github.com/opentelekomcloud-infra/system-config/playbooks/templates/github'
 headers = {
     'Authorization': f'token {github_token}',
     'Accept': 'application/vnd.github.luke-cage-preview+json'
@@ -18,6 +20,10 @@ bad_statuses = [400, 403, 404, 422]
 def read_yaml_file(path, org=None, endpoint=None, repo_name=None):
     if endpoint in ['manage_collaborators', 'branch_protection', 'options']:
         path += f'{org}/repositories/{repo_name}.yml'
+    if endpoint in ['teams']:
+        path += f'{org}/teams/members.yml'
+    if endpoint in ['members']:
+        path += f'{org}/people/members.yml'
     with open(path, 'r') as file:
         data = yaml.safe_load(file)
     return data
@@ -71,7 +77,8 @@ def update_branch_protection(github_api, owner, repo_name, repo):
     rules = repo[repo_name]['protection_rules']
     if isinstance(rules, str):
         rules = {repo[repo_name]['default_branch']:
-                 read_yaml_file(f'/home/zuul/src/github.com/opentelekomcloud-infra/system-config/playbooks/templates/github/{rules}.yaml')}
+            read_yaml_file(
+                f'{template_folder}/{rules}.yaml')}
     branch_name = list(rules)[0]
     if 'who_can_push' in rules[branch_name]:
         rules[branch_name]['restrictions'] = rules[branch_name].pop('who_can_push')
@@ -85,13 +92,86 @@ def update_branch_protection(github_api, owner, repo_name, repo):
     return print(output, file=sys.stderr)
 
 
+def update_org_members(github_api, owner, new_people):
+    output = ''
+    current_members = []
+
+    res = requests.get(f'{github_api}/orgs/{owner}/members')
+    org_members = json.loads(res.text)
+    for member in org_members:
+        current_members.append(member['login'])
+    for person in new_people['users']:
+        if person['login'] not in current_members:
+            res = requests.get(f'{github_api}/users/{person["login"]}')
+            user_id = json.loads(res.text)['id']
+            res = requests.post(
+                f'{github_api}/orgs/{owner}/invitations',
+                json={'invitee_id': user_id},
+                headers=headers,
+                timeout=15)
+            if res.status_code in bad_statuses:
+                output += f'invite not send for {person["login"]}' \
+                          f' status code: {res.status_code}, error is: {res.text}\n'
+    return print(output, file=sys.stderr)
+
+
+def update_teams(github_api, owner, new_teams):
+    output = ''
+    res = requests.get(
+        f'{github_api}/orgs/{owner}/teams',
+        headers=headers,
+        timeout=15)
+    teams = json.loads(res.text)
+    for team in teams:
+        members_to_add = new_teams['teams'][team['name']]
+        current_members = []
+        res = requests.get(
+            f'{github_api}/orgs/{owner}/teams/{team["slug"]}/members?role=member',
+            headers=headers,
+            timeout=15)
+        members = json.loads(res.text)
+        for member in members:
+            current_members.append(member['login'])
+
+        current_maintainers = []
+        res = requests.get(
+            f'{github_api}/orgs/{owner}/teams/{team["slug"]}/members?role=maintainer',
+            headers=headers,
+            timeout=15)
+        maintainers = json.loads(res.text)
+        for maintainer in maintainers:
+            current_maintainers.append(maintainer['login'])
+
+        for login in members_to_add['member']:
+            if login not in current_members:
+                res = requests.put(
+                    f'{github_api}/orgs/{owner}/teams/{team["slug"]}/memberships/{login}',
+                    json={'role': 'member'},
+                    headers=headers,
+                    timeout=15
+                )
+                if res.status_code in bad_statuses:
+                    output += f'membership not updated: {res.status_code}, error is: {res.text}\n'
+        for login in members_to_add['maintainer']:
+            if login not in current_maintainers:
+                res = requests.put(
+                    f'{github_api}/orgs/{owner}/teams/{team["slug"]}/memberships/{login}',
+                    json={'role': 'maintainer'},
+                    headers=headers,
+                    timeout=15)
+                if res.status_code in bad_statuses:
+                    output += f'membership not updated: {res.status_code}, error is: {res.text}\n'
+    return print(output, file=sys.stderr)
+
+
 if __name__ == '__main__':
     args_parser = ArgumentParser(prog='github_api', description='Multi-purpose github api script')
     args_parser.add_argument('--github_api_url', help='Github api base path', default='https://api.github.com')
     args_parser.add_argument('--endpoint', help='Selected github endpoint')
     args_parser.add_argument('--org', help='Repo owner')
     args_parser.add_argument('--repo', help='Repo data')
-    args_parser.add_argument('--root', help='Root directory to fetch files', default='/home/zuul/src/github.com/opentelekomcloud-infra/gitstyring/orgs/')
+    args_parser.add_argument('--root', help='Root directory to fetch files',
+                             default='/home/zuul/src/github.com/opentelekomcloud-infra/gitstyring/orgs/')
     args = args_parser.parse_args()
     if args.endpoint == 'manage_collaborators':
         manage_collaborators(
@@ -113,4 +193,16 @@ if __name__ == '__main__':
             owner=args.org,
             repo_name=args.repo,
             repo=read_yaml_file(path=args.root, org=args.org, repo_name=args.repo, endpoint=args.endpoint)
+        )
+    if args.endpoint == 'members':
+        update_org_members(
+            github_api=args.github_api_url,
+            owner=args.org,
+            new_people=read_yaml_file(path=args.root, org=args.org, endpoint=args.endpoint)
+        )
+    if args.endpoint == 'teams':
+        update_teams(
+            github_api=args.github_api_url,
+            owner=args.org,
+            new_teams=read_yaml_file(path=args.root, org=args.org, endpoint=args.endpoint)
         )
