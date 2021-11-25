f1b9679cc4b8940b534b36e629ff2963beafedcd
Kristian Kucerak <Kristian.Kucerak@t-systems.com>
2021-11-25 14:50:29 +0000

    added promtail gate group

--- testinfra/test_promtail.py
+++ testinfra/test_promtail.py
@@ -10,7 +10,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-testinfra_hosts = ['bridge.eco.tsi-dev.otc-service.com']
+testinfra_hosts = ['promtail.focal']
 
 def test_promtail_container_listening(host):
     sock = host.socket("tcp://0.0.0.0:3101")
