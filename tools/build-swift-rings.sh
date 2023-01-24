ca36de87fe89e2ef29b8fb1c1ffce816eb29c4ba
Artem Goncharov <Artem.goncharov@gmail.com>
2023-01-24 17:13:06 +0100

    remove storage2 and 3 from rings (#755)
    remove storage2 and 3 from rings
    
    Prepare replacement of storage2 and 3 nodes by dropping them from ring files first.
    
    Reviewed-by: Vladimir Vshivkov

--- tools/build-swift-rings.sh
+++ tools/build-swift-rings.sh
@@ -20,10 +20,10 @@ POLICIES="object container account"
 add_disk 1 101 101 vdd1
 
 # Zone 2
-add_disk 2 45 15 vdb
+#add_disk 2 45 15 vdb
 #
 # Zone 3
-add_disk 3 48 72 vdb
+#add_disk 3 48 72 vdb
 
 # Zone 4
 add_disk 4 104 104 vdd1
